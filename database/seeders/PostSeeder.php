652516f882b9d434d8a70a2b03947ebe315ca06f
Volodymyr Honcharov <ladimirvm87@gmail.com>
2026-02-24 12:59:49 +0200

    Use a JSON File to Read Data (for seeding db)

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -3,8 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Post;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class PostSeeder extends Seeder
 {
@@ -13,33 +14,17 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        $posts = collect([
-            [
-                'title' => 'Post One',
-                'slug' => 'post-one',
-                'excerpt' => 'Excerpt of post one',
-                'content' => 'Content of post one',
-                'is_published' => true,
-                'min_to_read' => 2,
-            ],
-            [
-                'title' => 'Post Two',
-                'slug' => 'post-two',
-                'excerpt' => 'Excerpt of post two',
-                'content' => 'Content of post two',
-                'is_published' => true,
-                'min_to_read' => 2,
-            ]
-        ]);
+        $json = File::get('database/json/posts.json');
+        $posts = collect(json_decode($json));
 
         $posts->each(function ($post) {
             Post::create([
-                'title' => $post['title'],
-                'slug' => $post['slug'],
-                'excerpt' => $post['excerpt'],
-                'content' => $post['content'],
-                'is_published' => $post['is_published'],
-                'min_to_read' => $post['min_to_read'],
+                'title' => $post->title,
+                'slug' => $post->slug,
+                'excerpt' => $post->excerpt,
+                'content' => $post->content,
+                'is_published' => $post->is_published,
+                'min_to_read' => $post->min_to_read,
             ]);
         });
     }
