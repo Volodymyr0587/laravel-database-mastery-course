21f71ae336aac0472e97ec195bfbf7df0d7f4134
Volodymyr Honcharov <ladimirvm87@gmail.com>
2026-03-05 15:38:30 +0200

    Cursor Pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+
 
 class PostController extends Controller
 {
@@ -14,8 +14,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::with('categories')
-            ->latest()
-            ->simplePaginate();
+            ->cursorPaginate(10);
 
         // dump($posts);
         return view('posts.index', ['posts' => $posts]);
