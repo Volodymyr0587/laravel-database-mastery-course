735d0f3f4fa34d7aa61a941090666a72ea86a89a
Volodymyr Honcharov <ladimirvm87@gmail.com>
2026-03-09 17:19:24 +0200

    Relationships: One to One

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use App\Models\Scopes\BalanceVerifiedScope;
 use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -47,6 +48,11 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function contact(): HasOne
+    {
+        return $this->hasOne(Contact::class);
     }
 
     /**
